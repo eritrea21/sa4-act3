a06c36e43077383754b3aa5d8f214f7e79d5c099
Mohammed Adem <mohjj46@email.com>
2024-02-07 20:46:19 -0600

    Modified code when q pressed  in the guess number tells the right guessed number

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,22 @@
-number = 10
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of ? "))
+import random 
 
-if guess == number:
-    print("Congratulations! You guessed the right number")
-else:
-    print(f"Sorry! The number was {number}.")
+def guess_number():
+    number = random.randint(1, 100)
+    while True:
+        guess = input("Guess a number between 1 and 100, or 'q' to quite: ")
+        if guess.lower()== 'q':
+            print(f"The number was {number}.")
+            break
+        try:
+            guess = int(guess)
+            if guess == number:
+                print("Congratulation! You guessed the correct number.")
+                break
+            else:
+                print("Incorrect guess. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number or 'q' to quit.")
+
+
+
+guess_number()
